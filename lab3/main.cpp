826d725d25d78beaa4d334a6f5ac0061de619ecd
Limonov Dmitriy <limonov2005@bk.ru>
2026-03-09 21:39:44 +0300

    lab3 Simpson + fixes

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -14,7 +14,7 @@ double calc_pr(double func(double), double a, double b, size_t n) {
     double h = (b - a) / n;
     double x = a;
     double s = 0;
-    for (int i = 0; i < n; i++) {
+    for (size_t i = 0; i < n; i++) {
         s += h * func(x + h / 2);
         x += h;
     }
@@ -23,13 +23,31 @@ double calc_pr(double func(double), double a, double b, size_t n) {
 
 double calc_tr(double func(double), double a, double b, size_t n) {
     double h = (b - a) / n;
-    double x = a;
+    double x = a + h;
     double s = (func(a) + func(b)) / 2;
-    for (int i = 0; i < n; i++) {
-        s += h * func(x);
+    for (size_t i = 1; i < n; i++) {
+        s += func(x);
         x += h;
     }
-    return s;
+    return s * h;
+}
+
+double calc_Sm(double func(double), double a, double b, size_t n) {
+    double s = 0;
+    double h = (b - a) / n;
+    double x = a + h;
+    for (size_t i = 1; i < n; i += 2) {
+        s += func(x) * 4;
+        x += h * 2;
+    }
+    x = a + 2 * h;
+    for (size_t i = 2; i < n; i += 2) {
+        s += func(x) * 2;
+        x += h * 2;
+    }
+    s += func(a);
+    s += func(b);
+    return s * h / 3;
 }
 
 struct answer {
@@ -65,13 +83,15 @@ int main() {
 
     double eps1 = 100;
     double eps2 = 100;
+    double eps3 = 100;
     bool end1 = false;
     bool end2 = false;
-    size_t count = 2;
+    bool end3 = false;
+    size_t count = 3;
     size_t step = 0;
     std::vector<answer> answers = {};
     while (count != 0) {
-        if (eps1 < EPSILON && !end1) {
+        if (std::abs(eps1) < EPSILON && !end1) {
             count--;
             double I = calc_pr(func, a, b, n);
             answer pr = {
@@ -80,20 +100,32 @@ int main() {
             };
             answers.push_back(pr);
             end1 = true;
-        } else if (eps1 > EPSILON) {
-            eps1 = std::abs((calc_pr(func, a, b, n) - calc_pr(func, a, b, n * 2)) / (std::pow(2, 2) - 1));
+        } else if (std::abs(eps1) > EPSILON) {
+            eps1 = (calc_pr(func, a, b, n * 2) - calc_pr(func, a, b, n)) / (std::pow(2, 2) - 1);
         }
-        if (eps2 < EPSILON && !end2) {
+        if (std::abs(eps2) < EPSILON && !end2) {
             count--;
             double I = calc_tr(func, a, b, n);
             answer tr = {
                 "Метод трапеций",
-                n, I, eps1, I + eps1
+                n, I, eps2, I + eps2
             };
             answers.push_back(tr);
             end2 = true;
-        } else if (eps2 > EPSILON) {
-            eps2 = std::abs((calc_tr(func, a, b, n / 2) - calc_tr(func, a, b, n)) / (std::pow(2, 2) - 1));
+        } else if (std::abs(eps2) > EPSILON) {
+            eps2 = (calc_tr(func, a, b, n * 2) - calc_tr(func, a, b, n)) / (std::pow(2, 2) - 1);
+        }
+        if (std::abs(eps3) < EPSILON && !end3) {
+            count--;
+            double I = calc_tr(func, a, b, n);
+            answer tr = {
+                "Метод Симпсона",
+                n, I, eps3, I + eps3
+            };
+            answers.push_back(tr);
+            end3 = true;
+        } else if (std::abs(eps3) > EPSILON) {
+            eps3 = (calc_Sm(func, a, b, n * 2) - calc_Sm(func, a, b, n)) / (std::pow(2, 4) - 1);
         }
         n *= 2;
         step += 1;
@@ -103,7 +135,7 @@ int main() {
         name_width = std::max(name_width, utf8_len(elem.name));
     }
 
-    print_padded_utf8("Название метода", name_width);
+    print_padded_utf8 ("Название метода", name_width);
     std::cout << " | " << std::left << std::setw(6) << "n" << " | " << std::setw(12) << "I*" << " | "
               << std::setw(12) << "R" << " | " << std::setw(12) << "I* + R" << " |\n";
     for (const auto& elem : answers) {
